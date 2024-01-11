e69736e97219563a36723f3c67cfc47f91c6b400
Laureen <laureen.pilo@gmail.com>
2024-01-11 14:29:45 +0100

    Exécution de l'app sur le serveur

--- backend/app.js
+++ backend/app.js
@@ -2,5 +2,10 @@
 const express = require ('express');
 // création de la constante app qui sera notre application, appeler la méthode express pour créer l'application express
 const app = express();
+// méthode use avec une fonction qui reçoit la requête et la réponse 
+// et utilisation de l'objet réponse avec la méthode json pour renvoyer une réponse en json
+app.use((req, res) => {
+    res.json({message: 'Votre requête a bien été reçue'});
+});
 // exporter l'application pour qu'on puisse y accéder depuis les autres fichiers du projet (le serveur node par exemple)
 module.exports = app;
